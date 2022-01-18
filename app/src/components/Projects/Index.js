b2a5846dfce38f3c7ed9451eed3ec2042086b70a
nicholas <n1ckorish14@gmail.com>
2022-01-18 18:30:51 +1100

    debugging project.js

--- app/src/components/Projects/Index.js
+++ app/src/components/Projects/Index.js
@@ -6,20 +6,23 @@ import "../../App.css"
 
 const ProjectCarousel = () => {
   return (
-    <Carousel fade className="projectCarousel">
-      {ProjectsArray.map((project) => (
-        <Carousel.Item key={project.id}>
-          <img className="d-block w-100" src={project.gif}  alt={project.name} />
-          <Carousel.Caption className="projectCaption">
-            <h3 className="projectName">{project.name}</h3>
-            <p>
-              GitHub <a href={project.gitHubLink} target="_blank" rel="noreferrer">repository</a>
-            </p>
-            <p>Project <a href={project.projectLink}>Link</a></p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      ))}
-    </Carousel>
+      <div>
+          <p>"hello error"</p>
+      </div>
+    // <Carousel fade className="projectCarousel">
+    //   {ProjectsArray.map((project) => (
+    //     <Carousel.Item key={project.id}>
+    //       <img className="d-block w-100" src={project.img}  alt={project.name} />
+    //       <Carousel.Caption className="projectCaption">
+    //         <h3 className="projectName">{project.name}</h3>
+    //         <p>
+    //           GitHub <a href={project.gitHubLink} target="_blank" rel="noreferrer">repository</a>
+    //         </p>
+    //         <p>Project <a href={project.projectLink}>Link</a></p>
+    //       </Carousel.Caption>
+    //     </Carousel.Item>
+    //   ))}
+    // </Carousel>
   );
 };
 
