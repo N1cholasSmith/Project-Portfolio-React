e310425368d13482d99213c2ab96dfefd3be316e
nicholas <n1ckorish14@gmail.com>
2022-01-18 19:01:32 +1100

    updated file structure

---
+++ src/App.js
@@ -0,0 +1,45 @@
+// import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+// import Routes from './components/Routes';
+import Navigation from './components/Navigation';
+import Home from './components/Home'
+import About from './components/About'
+import Services from './components/Services'
+import ProjectCarousel from './components/Projects/Index'
+import Contact from './components/Contact';
+import Footer from './components/Footer'
+
+
+function App() {
+  return (
+    <Router>
+      <Navigation />
+      <Switch>  
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/services">
+          <Services />
+        </Route>
+        <Route exact path="/projects">
+          <ProjectCarousel />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
+
+      <Footer />
+
+
+      {/* <Routes /> */}
+      {/* route component will show something different based on where you are  */}
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,72 @@
+import React from "react";
+import "../App.css";
+import Profilepic from "../assets/images/Profilepic.PNG"
+import Setup from "../assets/images/Setup.png"
+import Vizag from "../assets/images/Vizag.png"
+
+
+const Home = () => {
+    return (
+        <div>
+            <section id="home">
+                <div className="container mb-5">
+                <div className="row">
+                    <div className="col-sm-12 col-md-12 col-lg-6 d-flex flex-column justify-content-center">
+                    <h3 className="text-warning">SOFTWARE ENGINEER</h3>
+                    <h1 className="display-1 fw-bold">I'M NICHOLAS <br/> SMITH </h1>
+                    <div className="row buttons mt-4">
+                        <a className="col-3" href="assets/pdf/Nicholas-Smith-Software-Engineer-Brand-Statement.pdf"
+                        target="_blank"><button className=" btn btn-outline-warning px-4 py-2">Hire Me</button> </a>
+                        <a className="col-3" href="#about"> <button className="btn btn-warning text-white px-4 py-2">About</button> </a>
+                    </div>
+                    </div>
+                    <div className="col-sm-12 col-md-12 col-lg-6">
+                    <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+                        <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img src={Profilepic} className="d-block w-100" alt="office"
+                            height="600px"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img src={Setup} className="d-block w-100" alt="office"
+                            height="600px"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img src={Vizag} className="d-block w-100"
+                            alt="Nicholas at Vizag boys orphanage" height="600px"/>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </section>
+
+        </div>
+
+    );
+
+}
+
+export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
